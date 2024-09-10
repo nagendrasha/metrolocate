6556b9d2464c190fe0f6829b19d6c2162e0f8ae5
nagendra <nagendrasha191@gmail.com>
2024-09-10 23:06:42 +0530

    desigend completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   const [location, setLocation] = useState({ lat: "", long: "" });
   const [metroStation, setMetroStation] = useState(null);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -17,9 +19,10 @@ const App = () => {
       async (position) => {
         const lat = position.coords.latitude;
         const long = position.coords.longitude;
-        
+
         // Update location state with the user's current coordinates
         setLocation({ lat, long });
+        setIsLoading(true);
 
         const options = {
           method: "GET",
@@ -29,7 +32,8 @@ const App = () => {
             long,
           },
           headers: {
-            "x-rapidapi-key": "ddb8c1c205msh5a3972024ba2835p1cd237jsn893abf873908",
+            "x-rapidapi-key":
+              "ddb8c1c205msh5a3972024ba2835p1cd237jsn893abf873908",
             "x-rapidapi-host": "nearest-delhi-metro-station.p.rapidapi.com",
           },
         };
@@ -39,6 +43,7 @@ const App = () => {
           setMetroStation(response.data.data);
           console.log(response.data.data);
           setError(null);
+          setIsLoading(false);
         } catch (err) {
           setError("Error fetching metro station");
           setMetroStation(null);
@@ -51,48 +56,69 @@ const App = () => {
     );
   };
 
+
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Nearest Delhi Metro Station Finder</h1>
+    <>
+      <div className="container d-flex justify-content-center">
+        <div className="col-md-4 m-2 mt-5">
+          <div className="row card rounded-5 shadow p-5">
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+              <h2 className="text-center text-danger">
+                Nearest Delhi Metro Station Finder
+              </h2>
+              <div style={{ marginBottom: "20px" }}>
+                <label className="form-label">Latitude </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="lat"
+                  value={location.lat}
+                  onChange={handleInputChange}
+                  placeholder="Enter latitude"
+                  readOnly
+                />
+              </div>
 
-      <div style={{ marginBottom: "20px" }}>
-        <label>Latitude: </label>
-        <input
-          type="text"
-          name="lat"
-          value={location.lat}
-          onChange={handleInputChange}
-          placeholder="Enter latitude"
-          readOnly
-        />
-      </div>
+              <div style={{ marginBottom: "20px" }}>
+                <label className="form-label">Longitude </label>
+                <input
+                  type="text"
+                  name="long"
+                  className="form-control"
+                  value={location.long}
+                  onChange={handleInputChange}
+                  placeholder="Enter longitude"
+                  readOnly
+                />
+              </div>
 
-      <div style={{ marginBottom: "20px" }}>
-        <label>Longitude: </label>
-        <input
-          type="text"
-          name="long"
-          value={location.long}
-          onChange={handleInputChange}
-          placeholder="Enter longitude"
-          readOnly
-        />
-      </div>
+              <button className="btn btn-success" onClick={findNearestMetro}>
+                <i className="bi bi-train-front"> </i>
+                Find Nearest Metro
+              </button>
 
-      <button onClick={findNearestMetro}>Find Nearest Metro</button>
-
-      {metroStation && metroStation.length > 0 ? (
-        metroStation.map((e) => (
-          <div key={e.id}>
-            <h2>{e.station_name}</h2>
+              <div className="container-fluid">
+                {isLoading ? (
+                  <div className="spinner-border text-info mt-2" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </div>
+                ) : null}
+                {metroStation && metroStation.length > 0 ? (
+                  metroStation.slice(0, 6).map((e) => (
+                    <div key={e.id} className="mt-3 container">
+                      <h5>{e.station_name} </h5>
+                    </div>
+                  ))
+                ) : (
+                  <p className="text-danger">No metro stations found.</p>
+                )}
+              </div>
+              {error && <p style={{ color: "red" }}>{error}</p>}
+            </div>
           </div>
-        ))
-      ) : (
-        <p>No metro stations found.</p>
-      )}
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
-    </div>
+        </div>
+      </div>
+    </>
   );
 };
 
